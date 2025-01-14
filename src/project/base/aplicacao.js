670b7de2adfd817c4661a8b337362651c9108124
Lucas Teixeira Borges <lucastborges@gmail.com>
2025-01-13 22:50:30 -0300

    Add lights e envmap

--- src/project/base/aplicacao.js
+++ src/project/base/aplicacao.js
@@ -1,5 +1,5 @@
 
-import { THREE, FlyControls, GLTFLoader } from '../util/imports.js';
+import { THREE, FlyControls, GLTFLoader, RGBELoader, GroundedSkybox } from '../util/imports.js';
 import GuiManager from './guiManager.js';
 import Interface from './interface.js';
 import ThreeJsCanvas from '../components/threejsCanvas/threejsCanvas.js';
@@ -31,11 +31,22 @@ export default class Aplicacao {
 
     init(){
         this.loadingService.show();
+        const app = this;
         this.getModels().subscribe((main) => {
             this.sponza = main.scene;
+            const lights = main.parser.json.extensions?.KHR_lights_punctual?.lights || [];
+            this.lights = [];
+            lights.forEach((light, index) => {
+                const lightNode = main.scene.getObjectById(index);
+                if (lightNode) {
+                    app.lights.push(lightNode);
+                }
+            });
+            const start = new Date();
             this.makeScene();
             this.onInit.emit();
             this.loadingService.hide();
+            console.log("Cena carregada em "+(new Date().getTime()-start.getTime())/1000+" segundos");
         }).onFail((error) => {
             this.toastService.show("error","Erro ao carregar modelos",error.message);
             this.loadingService.hide();
@@ -46,18 +57,48 @@ export default class Aplicacao {
     makeScene() {
         this.canvas.show();
         this.camera = new THREE.PerspectiveCamera( 75, 1, 0.01, 80 );
-        this.camera.position.z = 0.01;
+        this.camera.position.set(10.88,1.31,-0.08);
+        this.setCamDirection(-0.97,0.26, 0.01)
         this.scene = new THREE.Scene();
-        /* const geometry = new THREE.PlaneGeometry( 1, 1 );
-        geometry.scale( 0.5, 0.5, 0.5 );
-        this.plane = new THREE.Mesh( new THREE.PlaneGeometry(), new THREE.MeshBasicMaterial( {color:0x444}) );
-        this.plane.position.set(0.5, 0.5, -0.5);
-        this.plane.name = "plano";
-        this.scene.add( this.plane ); */
+        const hdrLoader = new RGBELoader();
+        hdrLoader.loadAsync('/src/assets/810-hdri-skies-com.hdr').then((texture) => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        const skybox = new GroundedSkybox(texture,50,40);
+        skybox.position.y = 25;
+        this.scene.add(skybox);
+        });
+        this.sponza.children.forEach((child) => {
+            this.applyToGroup(child, (obj) => {
+                if(!obj.isLight){
+                    obj.castShadow = true;
+                    obj.receiveShadow = true;
+                }
+            });
+        });
         this.scene.add(this.sponza);
-        this.scene.add( new THREE.AmbientLight( 0xFFFFFF ) );
+        this.lights.forEach((light) => {
+            this.applyToGroup(light, (obj) => {
+            if(obj.isLight){
+                switch(obj.name){
+                    case 'SUN':
+                        obj.castShadow = true;
+                        obj.intensity = 5;
+                        obj.shadow.mapSize.width = 1024;
+                        obj.shadow.mapSize.height = 1024;
+                        break;
+                    default:
+                        obj.intensity = 5;
+                        obj.castShadow = false;
+                        break;
+                }
+                this.scene.add(obj);
+                console.log(obj);
+            }
+            });
+        });
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setPixelRatio( window.devicePixelRatio );
+        this.renderer.shadowMap.enabled = true;
         this.gui.show();
         this.onResize();
         this.renderer.setAnimationLoop(this.animate.bind(this));
@@ -77,6 +118,7 @@ export default class Aplicacao {
         this.renderer.setSize(dimensions.x, dimensions.y);
         this.guiComponent.fixPosition(dimensions);
         this.camera.aspect = dimensions.ratio;
+        this.camera.updateProjectionMatrix();
         this.onResizeEvent.emit(dimensions);
     }
 
@@ -101,7 +143,7 @@ export default class Aplicacao {
         const start = new Date();
         gltfLoader.load("/Assets/Models/glTF/Sponza-Intel/main1_sponza/NewSponza_Main_glTF_003.gltf",
             (gltf) => {
-                console.log("Modelo carregado em "+(new Date().getTime()-start.getTime())/1000+" segundos");
+                console.log("Modelo base carregado em "+(new Date().getTime()-start.getTime())/1000+" segundos");
                 main.emit(gltf);
             },
             (xhr) => {
@@ -111,5 +153,25 @@ export default class Aplicacao {
             }
         )
         return main;
+    }
+
+    setCamDirection(x,y,z){
+        let position = new THREE.Vector3();
+        this.camera.getWorldPosition(position);
+        if (!(x instanceof THREE.Vector3)) {
+            x = new THREE.Vector3(x, y, z);
+        }
+        this.camera.lookAt(position.add(x));
+    }
+
+    applyToGroup(group, func){
+        if (group.isGroup){
+            group.children.forEach((child) => {
+                this.applyToGroup(child,func);
+            });
+        } else {
+            func(group);
+        }
+
     }
 }
--- src/project/util/imports.js
+++ src/project/util/imports.js
@@ -3,4 +3,6 @@ import * as THREE from "/Assets/scripts/three.js/build/three.module.js"
 import GUI from "/Assets/scripts/three.js/examples/jsm/libs/lil-gui.module.min.js";
 import { FlyControls } from "/Assets/scripts/three.js/examples/jsm/controls/FlyControls.js";
 import { GLTFLoader } from "/Assets/scripts/three.js/examples/jsm/loaders/GLTFLoader.js";
-export { THREE, GUI, GLTFLoader, FlyControls};
+import { RGBELoader } from "/Assets/scripts/three.js/examples/jsm/loaders/RGBELoader.js";
+import { GroundedSkybox } from "/Assets/scripts/three.js/examples/jsm/objects/GroundedSkybox.js";
+export { THREE, GUI, GLTFLoader, FlyControls, RGBELoader, GroundedSkybox};
